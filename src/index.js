2423f28ec914e33475078d40a15d70865aaedd7e
Macarena Rivera <89401341+MacarenaRivera@users.noreply.github.com>
2022-02-16 10:50:45 -0300

    readme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./components/css/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "./components/context.jsx";
+import { Provider } from "./components/context";
 
 ReactDOM.render(
   <BrowserRouter>
